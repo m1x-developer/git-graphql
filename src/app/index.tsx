7f2ac935b81ea24d03dd77786bc64933db2148b8
Maks <m.korolev@cmsplanet.ru>
2023-08-22 20:05:13 +0300

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import { Root } from "./providers/Root.tsx";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Root />
+);
---
+++ src/app/providers/Root.tsx
@@ -0,0 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
+
+export const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>123</div>
+    </BrowserRouter>
+  );
+};
